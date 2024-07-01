e65fa3bd4a1c9e2a9aa7b3d7d561a8892c6944c8
deep patel <deep.jpatel71@gmail.com>
2024-07-01 19:14:02 -0400

    changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,11 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App';
-
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';  
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
+
